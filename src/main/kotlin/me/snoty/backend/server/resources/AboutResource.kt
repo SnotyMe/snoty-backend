cc736e8a0e4c74ffcca8413577ec374f816b91dd
SIMULATAN <simulatan2319@gmail.com>
2024-05-20 12:57:51 +0200

    About resource: add hostname and server time

--- src/main/kotlin/me/snoty/backend/server/resources/AboutResource.kt
+++ src/main/kotlin/me/snoty/backend/server/resources/AboutResource.kt
@@ -2,10 +2,26 @@ package me.snoty.backend.server.resources
 
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
 import me.snoty.backend.build.BuildInfo
+import java.net.InetAddress
+
+@Serializable
+data class AboutResponse(
+	val serverTime: Instant,
+	val hostname: String,
+	val buildInfo: BuildInfo
+)
 
 fun Route.aboutResource(buildInfo: BuildInfo) {
+	val hostname = InetAddress.getLocalHost().hostName
 	get("/info") {
-		call.respond(buildInfo)
+		call.respond(AboutResponse(
+			serverTime = Clock.System.now(),
+			hostname = hostname,
+			buildInfo = buildInfo
+		))
 	}
 }
