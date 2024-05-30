04955fc50ee5b577cf854fd427497a7472ddc847
SIMULATAN <simulatan2319@gmail.com>
2024-05-30 11:21:42 +0200

    Fix tests

--- src/test/kotlin/me/snoty/backend/test/TestScheduler.kt
+++ src/test/kotlin/me/snoty/backend/test/TestScheduler.kt
@@ -1,14 +1,14 @@
 package me.snoty.backend.test
 
-import me.snoty.backend.scheduling.JobRequest
 import me.snoty.backend.scheduling.Scheduler
+import me.snoty.backend.scheduling.SnotyJob
 
 class TestScheduler : Scheduler {
 	override fun scheduleJob(id: String, job: () -> Unit) {
 		job()
 	}
 
-	override fun scheduleJob(id: String, job: JobRequest) {
+	override fun scheduleJob(id: String, job: SnotyJob) {
 		throw NotImplementedError()
 	}
 }
