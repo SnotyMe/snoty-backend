b4ba815058bcc60b81195a62b16d1965f1f59b00
SIMULATAN <simulatan2319@gmail.com>
2024-08-05 21:29:41 +0200

    fixup! Add displayName and description to field metadata

--- integration-plugin/src/main/kotlin/me/snoty/integration/plugin/processor/NodeMetadataProcessor.kt
+++ integration-plugin/src/main/kotlin/me/snoty/integration/plugin/processor/NodeMetadataProcessor.kt
@@ -31,7 +31,7 @@ class NodeMetadataProcessor(private val logger: KSPLogger, private val codeGener
 
 	@OptIn(KspExperimental::class)
 	private fun processClass(resolver: Resolver, clazz: KSClassDeclaration) {
-		val node = clazz.getAnnotationsByType(RegisterNode::class).first()
+		val node = clazz.getAnnotationsByType(RegisterNode::class).single()
 		val displayName = node.displayName
 		val settingsClass = resolver.resolveClassFromAnnotation(clazz, RegisterNode::settingsType)
 		val inputClass = resolver.resolveClassFromAnnotation(clazz, RegisterNode::inputType)
