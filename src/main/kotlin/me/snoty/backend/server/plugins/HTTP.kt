c73c9f12e368f678de2894014675f643975614a3
SIMULATAN <simulatan2319@gmail.com>
2024-08-06 23:08:44 +0200

    Make CORS hosts configurable

--- src/main/kotlin/me/snoty/backend/server/plugins/HTTP.kt
+++ src/main/kotlin/me/snoty/backend/server/plugins/HTTP.kt
@@ -2,20 +2,20 @@ package me.snoty.backend.server.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.routing.CORS
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.plugins.forwardedheaders.*
-import me.snoty.backend.config.Environment
+import me.snoty.backend.config.Config
 
-fun Application.configureHTTP(environment: Environment) {
+fun Application.configureHTTP(config: Config) {
 	install(ForwardedHeaders) // WARNING: for security, do not include this if not behind a reverse proxy
 	install(XForwardedHeaders) // WARNING: for security, do not include this if not behind a reverse proxy
 	install(DefaultHeaders) {
 		header("X-Engine", "Ktor")
 	}
 	install(CORS) {
-		// TODO: configure allowed hosts in prod
-		if (environment.isDev()) anyHost()
+		if (config.environment.isDev()) anyHost()
+		hosts += config.corsHosts
 		HttpMethod.DefaultMethods.forEach(::allowMethod)
 		allowHeader(HttpHeaders.Authorization)
 		allowHeader(HttpHeaders.ContentType)
