d9b7eeb156dadfc19c488628ad794791266f0e4c
SIMULATAN <simulatan2319@gmail.com>
2024-08-07 21:22:46 +0200

    Rewrite `UserEntityStatesStatistics`

--- integrations/api/src/main/kotlin/me/snoty/integration/common/diff/state/UserEntityStatesStatistics.kt
+++ integrations/api/src/main/kotlin/me/snoty/integration/common/diff/state/UserEntityStatesStatistics.kt
@@ -1,21 +1,18 @@
 package me.snoty.integration.common.diff.state
 
-import com.mongodb.client.model.Accumulators
 import com.mongodb.client.model.Aggregates
 import com.mongodb.client.model.Projections
 import com.mongodb.kotlin.client.coroutine.AggregateFlow
 import me.snoty.backend.database.mongo.Stages
 import me.snoty.backend.database.mongo.aggregate
-import org.bson.BsonNull
 
-data class UserEntityStatesStats(val totalEntities: Long)
+data class UserEntityStateStats(val totalEntities: Long)
 
-// TODO: rewrite
-fun EntityStateCollection.getStatistics(): AggregateFlow<UserEntityStatesStats> {
+fun EntityStateCollection.getStatistics(): AggregateFlow<UserEntityStateStats> {
 	val entitiesValues = "entitiesValues"
 	val entityArray = "entityArray"
 
-	return aggregate<UserEntityStatesStats>(
+	return aggregate<UserEntityStateStats>(
 		Aggregates.project(
 			Projections.fields(
 				Projections.excludeId(),
@@ -24,15 +21,9 @@ fun EntityStateCollection.getStatistics(): AggregateFlow<UserEntityStatesStats> 
 		),
 		Aggregates.unwind("\$$entitiesValues"),
 		Aggregates.project(
-			Projections.fields(
-				Projections.computed(entityArray, "\$$entitiesValues.v")
-			)
+			Projections.computed(entityArray, "\$$entitiesValues.v")
 		),
 		Aggregates.unwind("\$${entityArray}"),
-		Aggregates.group(
-			// group everything into one result
-			BsonNull(),
-			Accumulators.sum(UserEntityStatesStats::totalEntities.name, 1)
-		)
+		Aggregates.count(UserEntityStateStats::totalEntities.name)
 	)
 }
