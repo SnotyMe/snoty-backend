be0089bcc5c73ca3afecc191b480909d0026f004
SIMULATAN <simulatan2319@gmail.com>
2024-09-10 22:44:49 +0200

    #20 store Flow logs per Execution / Job
    This allows us to set a TTL and prevents accumulating thousands of logs

--- src/test/kotlin/me/snoty/backend/test/TestFlowLogService.kt
+++ src/test/kotlin/me/snoty/backend/test/TestFlowLogService.kt
@@ -7,10 +7,10 @@ import me.snoty.integration.common.wiring.flow.NodeLogEntry
 class TestFlowLogService : FlowLogService {
 	private val logs = mutableMapOf<NodeId, MutableList<NodeLogEntry>>()
 
-	override suspend fun record(rootNode: NodeId, entry: NodeLogEntry) {
-		logs.getOrPut(rootNode) { mutableListOf() }.add(entry)
+	override suspend fun record(jobId: String, flowId: NodeId, entry: NodeLogEntry) {
+		logs.getOrPut(flowId) { mutableListOf() }.add(entry)
 	}
 
-	override suspend fun retrieve(rootNode: NodeId): List<NodeLogEntry>
-		= logs[rootNode] ?: emptyList()
+	override suspend fun retrieve(flowId: NodeId): List<NodeLogEntry>
+		= logs[flowId] ?: emptyList()
 }
