66db9b508bc6be718e8dbfa3aee90135270decdd
SIMULATAN <simulatan2319@gmail.com>
2024-09-21 10:09:58 +0200

    Fix UserEntityStatesStatistics

--- integrations/api/src/main/kotlin/me/snoty/integration/common/diff/state/UserEntityStatesStatistics.kt
+++ integrations/api/src/main/kotlin/me/snoty/integration/common/diff/state/UserEntityStatesStatistics.kt
@@ -1,29 +1,24 @@
 package me.snoty.integration.common.diff.state
 
+import com.mongodb.client.model.Accumulators
 import com.mongodb.client.model.Aggregates
 import com.mongodb.client.model.Projections
 import com.mongodb.kotlin.client.coroutine.AggregateFlow
-import me.snoty.backend.database.mongo.Stages
+import me.snoty.backend.database.mongo.Aggregations
 import me.snoty.backend.database.mongo.aggregate
+import me.snoty.backend.database.mongo.mongoField
 
 data class UserEntityStateStats(val totalEntities: Long)
 
-fun EntityStateCollection.getStatistics(): AggregateFlow<UserEntityStateStats> {
-	val entitiesValues = "entitiesValues"
-	val entityArray = "entityArray"
-
-	return aggregate<UserEntityStateStats>(
-		Aggregates.project(
-			Projections.fields(
-				Projections.excludeId(),
-				Projections.computed(entitiesValues, Stages.objectToArray(NodeEntityStates::entities.name))
-			)
-		),
-		Aggregates.unwind("\$$entitiesValues"),
-		Aggregates.project(
-			Projections.computed(entityArray, "\$$entitiesValues.v")
-		),
-		Aggregates.unwind("\$${entityArray}"),
-		Aggregates.count(UserEntityStateStats::totalEntities.name)
+fun EntityStateCollection.getStatistics(): AggregateFlow<UserEntityStateStats> = aggregate<UserEntityStateStats>(
+	Aggregates.project(
+		Projections.fields(
+			Projections.excludeId(),
+			Projections.computed(UserEntityStateStats::totalEntities.name, Aggregations.size(NodeEntityStates::entities))
+		)
+	),
+	Aggregates.group(
+		null,
+		Accumulators.sum(UserEntityStateStats::totalEntities.name, UserEntityStateStats::totalEntities.mongoField),
 	)
-}
+)
--- src/main/kotlin/me/snoty/backend/database/mongo/MongoExtensions.kt
+++ src/main/kotlin/me/snoty/backend/database/mongo/MongoExtensions.kt
@@ -1,10 +1,6 @@
 package me.snoty.backend.database.mongo
 
 import me.snoty.integration.common.wiring.node.NodeDescriptor
-import kotlin.reflect.KProperty
 
 val NodeDescriptor.mongoCollectionPrefix: String
 	get() = "$subsystem.$type"
-
-val KProperty<*>.mongoField
-	get() = "\$$name"
