fbcde157b4a4e265bb15bddc951297b3f4f98cea
SIMULATAN <simulatan2319@gmail.com>
2024-09-29 12:10:06 +0200

    #30 fix loading templates from JARs

--- integrations/api/src/main/kotlin/me/snoty/integration/common/wiring/node/template/NodeTemplateUtils.kt
+++ integrations/api/src/main/kotlin/me/snoty/integration/common/wiring/node/template/NodeTemplateUtils.kt
@@ -1,20 +1,47 @@
 package me.snoty.integration.common.wiring.node.template
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import me.snoty.integration.common.wiring.node.NodeDescriptor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import java.io.File
+import java.nio.file.FileSystems
+import java.nio.file.Path
+import kotlin.io.path.*
 
 object NodeTemplateUtils {
-	private fun provideNodeTemplates(descriptor: NodeDescriptor) =
-		javaClass.getResource("/node/${descriptor.subsystem}/${descriptor.type}")
-			?.let { File(it.file) }
-			?.walk()
-			?.filter { it.isFile && it.extension == "liquid" }
+	private val logger = KotlinLogging.logger {}
+
+	@OptIn(ExperimentalPathApi::class)
+	private fun provideNodeTemplates(descriptor: NodeDescriptor): Sequence<Path>? {
+		val nodeDirectory = "/node/${descriptor.subsystem}/${descriptor.type}"
+		val resource = javaClass.getResource(nodeDirectory) ?: return let {
+			logger.trace { "No node directory found for ${descriptor.id}" }
+			null
+		}
+
+		val root = when {
+			resource.path.contains(".jar!") -> {
+				logger.trace { "Running in JAR, resolving using FileSystems" }
+				val path = Path.of(resource.path.substringBefore("!").substringAfter("file:"))
+				FileSystems.newFileSystem(path).getPath(nodeDirectory)
+			}
+			else -> {
+				logger.trace { "Resource is on the classpath, resolving using a regular walk" }
+				Path.of(resource.path)
+			}
+		}
+
+		return root.walk().filter {
+			logger.trace { "Checking file ${it.name} for ${descriptor.id}" }
+			it.isRegularFile() && it.extension == "liquid"
+		}
+	}
 
 	fun nodeTemplatesModule(descriptor: NodeDescriptor) = module {
 		provideNodeTemplates(descriptor)
 			?.forEach { templateFile ->
+				logger.debug { "Registering template ${templateFile.nameWithoutExtension} for ${descriptor.id}" }
+
 				val cached = NodeTemplate(
 					node = descriptor,
 					name = templateFile.nameWithoutExtension,
@@ -29,6 +56,6 @@ object NodeTemplateUtils {
 						cached.copy(template = templateFile.readText())
 					}
 				}
-			}
+			} ?: logger.trace { "Found no templates for ${descriptor.id}" }
 	}
 }
