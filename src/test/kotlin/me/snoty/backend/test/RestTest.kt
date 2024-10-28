d1430a448c078e44628fa138c446c46a8d147526
SIMULATAN <simulatan2319@gmail.com>
2024-10-28 17:04:10 +0100

    Group system + job admin tasks
    Closes #10

--- src/test/kotlin/me/snoty/backend/test/RestTest.kt
+++ src/test/kotlin/me/snoty/backend/test/RestTest.kt
@@ -2,11 +2,13 @@ package me.snoty.backend.test
 
 import io.ktor.server.routing.*
 import io.ktor.server.testing.*
+import io.mockk.mockk
 import me.snoty.backend.config.Config
 import me.snoty.backend.server.plugins.configureRouting
 import me.snoty.backend.server.plugins.configureSecurity
 import me.snoty.backend.server.plugins.configureSerialization
 import me.snoty.integration.common.BaseSnotyJson
+import me.snoty.integration.common.wiring.httpClient
 
 fun ktorApplicationTest(
 	config: Config = TestConfig,
@@ -16,7 +18,7 @@ fun ktorApplicationTest(
 	testApplication {
 		application {
 			configureSerialization(BaseSnotyJson)
-			configureSecurity(config)
+			configureSecurity(config, httpClient(mockk(relaxed = true)))
 			configureRouting(config)
 		}
 
