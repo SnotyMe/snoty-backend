2701ee992d3dc47e6f4603d8e6f4726ecca3b47c
SIMULATAN <jakob@simulatan.me>
2024-12-15 17:31:41 +0100

    fix(tests): #108 exclude nonstandard sourceSets from test coverage
    Fixes #108

--- conventions/project/src/main/kotlin/snoty.kotlin-conventions.gradle.kts
+++ conventions/project/src/main/kotlin/snoty.kotlin-conventions.gradle.kts
@@ -15,14 +15,17 @@ kover {
 		allProjects()
 	}
 	currentProject {
-		sources {
-			// per default, kover only excludes `test`
-			// since we also have `testIntegration`, we have to exclude it
-			testing.suites
-				.filterIsInstance<JvmTestSuite>()
-				.forEach {
-					excludedSourceSets.add(it.sources.name)
-				}
+		// awaiting implementation of https://github.com/Kotlin/kotlinx-kover/issues/714
+		afterEvaluate {
+			sources {
+				// per default, kover only excludes `test`
+				// since we also have `testIntegration` and `dev`, we have to exclude them
+				sourceSets
+					.filter { it.name != sourceSets.main.name }
+					.forEach {
+						excludedSourceSets.add(it.name)
+					}
+			}
 		}
 	}
 }
