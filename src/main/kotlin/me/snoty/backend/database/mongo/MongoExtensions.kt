c2ff18603938aadac3a88fc13ee20e2ce68e697b
SIMULATAN <jakob@simulatan.me>
2024-12-16 18:58:48 +0100

    MongoDB: use `:` as the separator between the NodeDescriptor parts
    This aligns the code with the migration script

--- src/main/kotlin/me/snoty/backend/database/mongo/MongoExtensions.kt
+++ src/main/kotlin/me/snoty/backend/database/mongo/MongoExtensions.kt
@@ -3,4 +3,4 @@ package me.snoty.backend.database.mongo
 import me.snoty.integration.common.wiring.node.NodeDescriptor
 
 val NodeDescriptor.mongoCollectionPrefix: String
-	get() = "nodes:$namespace.$name"
+	get() = "nodes:$namespace:$name"
