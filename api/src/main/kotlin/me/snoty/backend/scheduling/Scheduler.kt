0dc58e002e7f37a8b103b04b10b21757a71a91d3
SIMULATAN <jakob@simulatan.me>
2024-12-20 21:49:42 +0100

    Move JobRunr startup between Migrations and Ktor launch

--- api/src/main/kotlin/me/snoty/backend/scheduling/Scheduler.kt
+++ api/src/main/kotlin/me/snoty/backend/scheduling/Scheduler.kt
@@ -1,6 +1,9 @@
 package me.snoty.backend.scheduling
 
 interface Scheduler {
+	fun start()
+
 	fun scheduleJob(job: SnotyJob)
 	fun scheduleRecurringJob(id: String, job: SnotyJob)
+	fun deleteRecurringJob(id: String)
 }
