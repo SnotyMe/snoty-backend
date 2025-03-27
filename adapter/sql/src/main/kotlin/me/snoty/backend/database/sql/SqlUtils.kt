667e4498698759a4556cf21af39349c2b1474d4f
SIMULATAN <jakob@simulatan.me>
2025-03-27 19:31:39 +0100

    fix(sql): connection leaks in `SqlFlowService#getWithNodes`

--- adapter/sql/src/main/kotlin/me/snoty/backend/database/sql/SqlUtils.kt
+++ adapter/sql/src/main/kotlin/me/snoty/backend/database/sql/SqlUtils.kt
@@ -1,19 +1,17 @@
 package me.snoty.backend.database.sql
 
-import me.snoty.backend.utils.flowOf
 import me.snoty.backend.utils.flowOfEach
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.experimental.withSuspendTransaction
+import org.jetbrains.exposed.sql.transactions.transactionManager
 
 suspend inline fun <T> Database.newSuspendedTransaction(noinline statement: suspend Transaction.() -> T)
-	= newSuspendedTransaction(db = this, statement = statement)
-
-fun <T> Database.flowTransaction(statement: suspend Transaction.() -> T) = flowOf<T> {
-	this.newSuspendedTransaction(statement)
-}
+	= transactionManager.currentOrNull()?.withSuspendTransaction(statement = statement)
+		?: newSuspendedTransaction(db = this, statement = statement)
 
 @JvmName("flowTransactionCollection")
-fun <T> Database.flowTransaction(statement: suspend Transaction.() -> Collection<T>) = flowOfEach<T> {
+fun <T> Database.flowTransaction(statement: suspend Transaction.() -> Collection<T>) = flowOfEach {
 	this.newSuspendedTransaction(statement)
 }
