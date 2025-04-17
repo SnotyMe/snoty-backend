05af0fae82f4509de2c7352a15c41cef81228316
SIMULATAN <jakob@simulatan.me>
2025-04-18 01:57:23 +0200

    feat(Mapper): support setting list indexes

--- api/src/main/kotlin/me/snoty/backend/utils/bson/BsonUtils.kt
+++ api/src/main/kotlin/me/snoty/backend/utils/bson/BsonUtils.kt
@@ -47,16 +47,45 @@ fun Document.getIdAsString(): String? = when (val id = get("id")) {
 
 fun Document.setRecursively(key: String, value: Any?) {
 	val parts = key.split(".")
-	parts.dropLast(1).fold(this) { acc, part ->
+	if (parts.isEmpty()) throw IllegalArgumentException("Key must not be empty")
+
+	val lastParent = getLastParent(parts)
+
+	val lastPart = parts.last()
+	when {
+		// array index
+		lastPart.matches("\\w+\\[\\d+]".toRegex())
+			-> lastParent.setListIndex(lastPart, value)
+		else -> lastParent[lastPart] = value
+	}
+}
+
+private fun Document.setListIndex(specifier: String, value: Any?) {
+	val key = specifier.substringBefore("[")
+	val index = specifier.substringAfter("[").substringBefore("]").toInt()
+	val list = when {
+		containsKey(key) -> getList(key, Any::class.java)
+		else -> {
+			val newList = mutableListOf<Any?>()
+			this[key] = newList
+			newList
+		}
+	}
+
+	if (index >= list.size) {
+		// list containing nulls until the specified index
+		val fillerList = List(index - list.size) { null }
+		list.addAll(fillerList)
+		list.add(value)
+	} else {
+		list[index] = value
+	}
+}
+
+private fun Document.getLastParent(parts: List<String>): Document = parts
+	.dropLast(1)
+	.fold(this) { acc, part ->
 		val next = acc[part] as? Document ?: Document()
 		acc[part] = next
 		next
-	}[parts.last()] = value
-}
-
-fun Document.getRecursively(key: String): Any? {
-	val parts = key.split(".")
-	return parts.dropLast(1).fold(this) { acc, part ->
-		acc.get(part, Document::class.java)
-	}[parts.last()]
-}
+	}
--- integrations/builtin/src/test/kotlin/me/snoty/integration/builtin/mapper/MapperEngineTests.kt
+++ integrations/builtin/src/test/kotlin/me/snoty/integration/builtin/mapper/MapperEngineTests.kt
@@ -1,7 +1,8 @@
 package me.snoty.integration.builtin.mapper
 
 import org.bson.Document
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 
@@ -12,6 +13,38 @@ abstract class MapperEngineTest(val engine: MapperEngine) {
 
 	abstract fun simple()
 	abstract fun dotToMap()
+
+	@Test
+	fun map() {
+		val output = engine.template(logger, settings("my.key" to "value"), Document())
+		assertEquals(Document("my", Document("key", "value")), output)
+	}
+
+	@Test
+	fun list() {
+		val output = engine.template(logger, settings("my.key[0]" to "value", "my.key[1]" to "value2"), Document())
+
+		assertTrue(output.containsKey("my"))
+		val subdoc = output.get("my", Document::class.java)
+		assertTrue(subdoc.containsKey("key"))
+		val keyList = subdoc.get("key", List::class.java)
+		assertEquals(2, keyList.size)
+		assertEquals("value", keyList[0])
+		assertEquals("value2", keyList[1])
+
+		val output2 = engine.template(logger, settings("key[0]" to "value", "key[1]" to "value2"), Document())
+		assertTrue(output2.containsKey("key"))
+		val keyList2 = output2.get("key", List::class.java)
+		assertEquals(2, keyList2.size)
+		assertEquals("value", keyList2[0])
+		assertEquals("value2", keyList2[1])
+
+		val output3 = engine.template(logger, settings("key[3]" to "value3", "key[1]" to "value1"), Document())
+		assertTrue(output3.containsKey("key"))
+		val keyList3 = output3.get("key", List::class.java)
+		assertEquals("value3", keyList3[3])
+		assertEquals("value1", keyList3[1])
+	}
 
 	class Liquid : MapperEngineTest(MapperEngine.LIQUID) {
 		@Test
