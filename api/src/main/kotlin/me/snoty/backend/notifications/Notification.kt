1472334dba37f6c7733647ede0131202cd9c378f
SIMULATAN <jakob@simulatan.me>
2025-04-22 21:08:57 +0200

    feat(notification): send Flow failure notifications

--- api/src/main/kotlin/me/snoty/backend/notifications/Notification.kt
+++ api/src/main/kotlin/me/snoty/backend/notifications/Notification.kt
@@ -2,8 +2,42 @@ package me.snoty.backend.notifications
 
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
+import me.snoty.backend.observability.FLOW_ID
+import me.snoty.backend.observability.NODE_ID
 
 typealias NotificationAttributes = Map<String, String>
+
+const val NOTIFICATION_TYPE = "notificationType"
+
+fun NotificationAttributes(
+	type: String,
+	flowId: String,
+	nodeId: String,
+	vararg extra: Pair<String, String>,
+) = NotificationAttributes(
+	type,
+	flowId,
+	NODE_ID.key to nodeId,
+	*extra,
+)
+
+fun NotificationAttributes(
+	type: String,
+	flowId: String,
+	vararg extra: Pair<String, String>,
+): NotificationAttributes = NotificationAttributes(
+	type,
+	FLOW_ID.key to flowId,
+	*extra
+)
+
+fun NotificationAttributes(
+	type: String,
+	vararg extra: Pair<String, String>,
+): NotificationAttributes = mapOf(
+	NOTIFICATION_TYPE to type,
+	*extra,
+)
 
 @Serializable
 data class Notification(
@@ -12,4 +46,7 @@ data class Notification(
 	val resolvedAt: Instant? = null,
 	val lastSeenAt: Instant,
 	val count: Int,
+
+	val title: String,
+	val description: String? = null,
 )
