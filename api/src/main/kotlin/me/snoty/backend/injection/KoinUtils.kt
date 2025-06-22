77df9ee76957eb29c574953c9d23bfe1d9c9a81c
SIMULATAN <jakob@simulatan.me>
2025-06-22 11:07:44 +0200

    fix: `Koin.getFromAllScopes` returns duplicate instances

--- api/src/main/kotlin/me/snoty/backend/injection/KoinUtils.kt
+++ api/src/main/kotlin/me/snoty/backend/injection/KoinUtils.kt
@@ -4,8 +4,9 @@ import org.koin.core.Koin
 import org.koin.core.annotation.KoinInternalApi
 
 @OptIn(KoinInternalApi::class)
-inline fun <reified T : Any> Koin.getFromAllScopes(): List<T> = this.scopeRegistry.scopeDefinitions.map {
-	this.getOrCreateScope(it.value, it)
-}.flatMap {
-	it.getAll()
-}
+inline fun <reified T : Any> Koin.getFromAllScopes(): List<T> =
+	scopeRegistry.scopeDefinitions.map {
+		getOrCreateScope(it.value, it)
+	}
+		.flatMap { it.getAll<T>() }
+		.distinct()
