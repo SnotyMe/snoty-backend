549f4d2291095fb8ac65220469df451abd0710b7
SIMULATAN <jakob@simulatan.me>
2025-08-13 18:36:09 +0200

    fix(runner): log output for orphaned START Nodes too
    The original commit, 3edf01d113533bbb30e78d299dae349fcc171a9c, limited this behavior to MIDDLE Nodes

--- api/src/main/kotlin/me/snoty/integration/common/model/NodePosition.kt
+++ api/src/main/kotlin/me/snoty/integration/common/model/NodePosition.kt
@@ -1,7 +1,15 @@
 package me.snoty.integration.common.model
 
-enum class NodePosition {
-	START,
-	MIDDLE,
-	END
+enum class NodePosition(
+	val logOutput: Boolean,
+) {
+	START(
+		logOutput = true
+	),
+	MIDDLE(
+		logOutput = true
+	),
+	END(
+		logOutput = false
+	)
 }
--- src/main/kotlin/me/snoty/backend/integration/flow/execution/FlowRunnerImpl.kt
+++ src/main/kotlin/me/snoty/backend/integration/flow/execution/FlowRunnerImpl.kt
@@ -177,7 +177,7 @@ class FlowRunnerImpl(
 			// pls fix Kotlin
 			val data = with(context) { with(handler) { process(node, input) } }
 			logger.debug { "Processed ${node.descriptor.name} node \"${node.settings.name}\" (${node._id})" }
-			if (nodeRegistry.getMetadata(node.descriptor).position == NodePosition.MIDDLE && node.next.isEmpty()) {
+			if (nodeRegistry.getMetadata(node.descriptor).position.logOutput && node.next.isEmpty()) {
 				logger.debug { "Node \"${node.settings.name}\" (${node._id}) has no output nodes, would have emitted $data" }
 			}
 
