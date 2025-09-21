1e5b5ec48d9ec0365f071c18bd32da7a19cc162e
SIMULATAN <jakob@simulatan.me>
2025-09-21 11:14:42 +0200

    feat(build): replace hard-coded repository
    Builds in forks will now have correct references

--- conventions/project/src/main/kotlin/snoty.jib-conventions.gradle.kts
+++ conventions/project/src/main/kotlin/snoty.jib-conventions.gradle.kts
@@ -11,7 +11,9 @@ jib {
 	to {
 		val allTags = project.properties["me.snoty.docker.tags"]?.toString()?.trim()?.split(" ")?.toSet()
 			?: setOf(version.toString())
-		image = "ghcr.io/snotyme/snoty-backend:${allTags.first()}"
+		val imageName = project.properties["me.snoty.docker.image"]?.toString()?.trim()
+			?: "ghcr.io/snotyme/snoty-backend"
+		image = "$imageName:${allTags.first()}"
 		// workaround for the TERRIBLE design decisions of the JIB developers to
 		// still generate the `latest` tag even when tags are specified...
 		if (allTags.size > 1) {
@@ -25,28 +27,29 @@ jib {
 		appRoot = "/app"
 		workingDirectory = "/app"
 		ports = listOf("8080")
+
 		val (ghaRunId, ghaRunNumber) =
 			project.properties["me.snoty.github.run"]?.toString()?.split(":") ?: listOf(null, null)
-		labels = mapOf(
-			"org.opencontainers.image.title" to "snoty-backend",
-			"org.opencontainers.image.description" to "Backend for the snoty project",
-			"org.opencontainers.image.url" to "https://github.com/SnotyMe/snoty-backend/pkgs/container/snoty-backend",
-			*Git.open(project.rootDir).use { git ->
-				val headRef = git.repository.resolve("HEAD").name
-				arrayOf(
-					"org.opencontainers.image.revision" to headRef,
-					// source to https version of the git repository
-					"org.opencontainers.image.source" to git.repository.config.getString("remote", "origin", "url")
-						.replace(":", "/")
-						.replace("git@", "https://")
-						.replace(".git", "")
-						+ "/tree/$headRef"
-				)
-			},
-			*if (ghaRunId != null && ghaRunNumber != null) arrayOf(
-				"com.github.actions.run.id" to ghaRunId,
-				"com.github.actions.run.number" to ghaRunNumber
-			) else arrayOf()
-		)
+
+		Git.open(project.rootDir).use { git ->
+			val repoUrl = git.repository.config
+				.getString("remote", "origin", "url")
+				.replace(":", "/")
+				.replace("git@", "https://")
+				.replace(".git", "")
+			val headRef = git.repository.resolve("HEAD").name
+
+			labels = mapOf(
+				"org.opencontainers.image.title" to "snoty-backend",
+				"org.opencontainers.image.description" to "Backend for the snoty project",
+				"org.opencontainers.image.url" to "$repoUrl/pkgs/container/snoty-backend",
+				"org.opencontainers.image.revision" to headRef,
+				"org.opencontainers.image.source" to "$repoUrl/tree/$headRef",
+				*if (ghaRunId != null && ghaRunNumber != null) arrayOf(
+					"com.github.actions.run.id" to ghaRunId,
+					"com.github.actions.run.number" to ghaRunNumber
+				) else arrayOf()
+			)
+		}
 	}
 }
